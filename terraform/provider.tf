8158ea3606eac9c225e2b8276101911829dc27eb
artem <artem@MacBook-Air-artem.local>
2024-01-18 01:45:49 +0300

    docker build

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,15 +9,9 @@ terraform {
 
 
 
-
 provider "yandex" {
   service_account_key_file = "/Users/artem/key.json"
   zone = "ru-central1-a"
   cloud_id = "cloud-temalol13"
   folder_id = "b1gehbt0k7uoou9p1vt9"
-}
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "yc-k8s-regional"
 }
