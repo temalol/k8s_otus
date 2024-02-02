fbd7aa9f7e4c3cc0efa351b2e6fded9d7564f1c9
artem <artem@MacBook-Air-artem.local>
2024-02-03 01:15:46 +0300

    dash

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -1,13 +1,6 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/kubernetes" {
-  version = "2.25.2"
-  hashes = [
-    "h1:o/+UcYEaEHrQzq2kkWw2MohCK033u6vY+T6cmHd46QU=",
-  ]
-}
-
 provider "registry.terraform.io/yandex-cloud/yandex" {
   version = "0.105.0"
   hashes = [
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,21 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+  
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "terraformstateotus"
+    region = "ru-central1"
+    key    = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
 }
 
 
@@ -13,3 +28,5 @@ provider "yandex" {
   service_account_key_file = "/Users/artem/key.json"
   cloud_id = "cloud-temalol13"
 }
+
+
