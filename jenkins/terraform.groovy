36b30f3bf5b48d83a79121c47d57c12a98806354
artem <artem@MacBook-Air-artem.local>
2024-02-04 20:03:17 +0300

    dash

--- jenkins/terraform.groovy
+++ jenkins/terraform.groovy
@@ -14,8 +14,8 @@ pipeline {
         stage('Terraform apply') {
             steps {
                dir('terraform') {
-                    //sh "terraform providers lock -net-mirror=https://terraform-mirror.yandexcloud.net -platform=linux_amd64 -platform=darwin_arm64 yandex-cloud/yandex"
                     sh "terraform init -input=false"
+                }
             }
         }
     }
