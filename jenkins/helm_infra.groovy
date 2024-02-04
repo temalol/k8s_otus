209c8a64e30e5f8d7f7a570e07f57ccbb5f11181
artem <artem@MacBook-Air-artem.local>
2024-02-04 23:14:36 +0300

    dash

--- jenkins/helm_infra.groovy
+++ jenkins/helm_infra.groovy
@@ -19,7 +19,7 @@ pipeline {
                         writeFile(file: "private.gpg", text: "${helm_gpg_private}")
                         writeFile(file: "public.gpg", text: "${helm_gpg_public}")
                         }
-                        println (${helm_gpg_private})
+                        println ("${helm_gpg_private}")
                         sh "gpg --import public.gpg"
                         sh "gpg --import private.gpg"
                         sh "helm plugin list"
