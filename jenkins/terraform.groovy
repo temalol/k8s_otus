966009e1d204dfafae7b1d72a1a67c7a0d3a7460
artem <artem@MacBook-Air-artem.local>
2024-02-04 19:43:59 +0300

    dash

--- jenkins/terraform.groovy
+++ jenkins/terraform.groovy
@@ -13,7 +13,7 @@ pipeline {
         stage('Terraform apply') {
             steps {
                dir('terraform') {
-                    sh "terraform init -migrate-state -backend-config=access_key=$AWS_ACCESS_KEY_ID -backend-config=secret_key=$AWS_SECRET_ACCESS_KEY"
+                    sh "terraform init -migrate-state -backend-config=access_key=\"$AWS_ACCESS_KEY_ID\" -backend-config=secret_key=\"$AWS_SECRET_ACCESS_KEY\""
                }
             }
         }
