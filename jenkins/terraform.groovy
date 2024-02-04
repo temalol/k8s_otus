257ef82c47295188c75807eaa5f68e8b61ebec1d
artem <artem@MacBook-Air-artem.local>
2024-02-04 18:39:20 +0300

    dash

--- jenkins/terraform.groovy
+++ jenkins/terraform.groovy
@@ -4,12 +4,15 @@ pipeline {
     parameters { 
         choice(name: 'TERRAFORM_OPTS', choices: ['destroy', 'apply'], description: '')
     }
-
+    environment {
+        AWS_ACCESS_KEY_ID     = credentials('s3-terraform-secret-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('s3-terraform-secret-access-key')
+    }    
     stages {
         stage('Terraform apply') {
             steps {
                dir('terraform') {
-                    sh "terraform ${TERRAFORM_OPTS} -auto-approve"
+                    sh "terraform -reconfigure ${TERRAFORM_OPTS} -auto-approve"
                }
             }
         }
