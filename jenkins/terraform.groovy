67da5591a6d35ebde8f2e0bbd2fd5c72d834dc52
artem <artem@MacBook-Air-artem.local>
2024-02-04 18:32:34 +0300

    dash

--- jenkins/terraform.groovy
+++ jenkins/terraform.groovy
@@ -1,10 +1,16 @@
 pipeline {
     agent any
 
+    parameters { 
+        choice(name: 'TERRAFORM_OPTS', choices: ['destroy', 'apply'], description: '')
+    }
+
     stages {
         stage('Terraform apply') {
             steps {
-                echo 'apply..'
+               dir('terraform') {
+                    sh "terraform ${TERRAFORM_OPTS} -auto-approve"
+               }
             }
         }
     }
