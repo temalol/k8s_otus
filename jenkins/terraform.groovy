6e77e8300dcae98e971759705e95b4263f8e2a7a
artem <artem@MacBook-Air-artem.local>
2024-02-04 19:15:49 +0300

    dash

--- jenkins/terraform.groovy
+++ jenkins/terraform.groovy
@@ -7,12 +7,13 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID     = credentials('s3-terraform-secret-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('s3-terraform-secret-access-key')
+        TF_CLI_CONFIG_FILE = 
     }    
     stages {
         stage('Terraform apply') {
             steps {
                dir('terraform') {
-                    sh "terraform providers lock -net-mirror=https://terraform-mirror.yandexcloud.net -platform=linux_amd64 -platform=darwin_arm64 yandex-cloud/yandex"
+                    //sh "terraform providers lock -net-mirror=https://terraform-mirror.yandexcloud.net -platform=linux_amd64 -platform=darwin_arm64 yandex-cloud/yandex"
                     sh "terraform init -migrate-state"
                }
             }
