e4bbe2bba3c7c8b2e8c1c394147074a22ac8ff14
artem <artem@MacBook-Air-artem.local>
2024-02-05 17:58:51 +0300

    dash

---
+++ etc/get_kubeconfig.sh
@@ -0,0 +1,40 @@
+CLUSTER_ID=cat0jfiq9bcuu22beu3r
+
+
+yc managed-kubernetes cluster get --id $CLUSTER_ID --format json | \
+  jq -r .master.master_auth.cluster_ca_certificate | \
+  awk '{gsub(/\\n/,"\n")}1' > ca.pem
+
+kubectl create -f sa.yaml -f sa.yaml
+
+
+SA_TOKEN=$(kubectl -n kube-system get secret $(kubectl -n kube-system get secret | \
+  grep admin-user-token | \
+  awk '{print $1}') -o json | \
+  jq -r .data.token | \
+  base64 -d)
+
+MASTER_ENDPOINT=$(yc managed-kubernetes cluster get --id $CLUSTER_ID \
+  --format json | \
+  jq -r .master.endpoints.external_v4_endpoint)
+
+
+kubectl config set-cluster sa-test2 \
+  --certificate-authority=ca.pem \
+  --server=$MASTER_ENDPOINT \
+  --kubeconfig=test.kubeconfig
+
+kubectl config set-credentials admin-user \
+  --token=$SA_TOKEN \
+  --kubeconfig=test.kubeconfig
+
+
+
+kubectl config set-context default \
+  --cluster=sa-test2 \
+  --user=admin-user \
+  --kubeconfig=test.kubeconfig
+
+kubectl config use-context default \
+  --kubeconfig=test.kubeconfig
+
