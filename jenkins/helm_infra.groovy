f570224ac974d6ad15fd60fa9964684efb07e4d9
artem <artem@MacBook-Air-artem.local>
2024-02-05 14:40:30 +0300

    dash

--- jenkins/helm_infra.groovy
+++ jenkins/helm_infra.groovy
@@ -5,22 +5,22 @@ pipeline {
         ansiColor('xterm')
         timeout(time: 1, unit: 'HOURS') 
     }
+    environment {
+        KUBECONFIG = './kubeconfig'
+    }
     stages {
         stage("Run helmfile") {
             steps {
                dir('helmfile_infra') {
                     withCredentials([file(credentialsId: 'kubeconfig', variable: 'kubeconfig'),
                         file(credentialsId: 'gpg_private', variable: 'helm_gpg_private')]) {
-                                    
                         sh "gpg --batch --allow-secret-key-import --import ${helm_gpg_private}"
                         //sh "helm plugin install https://github.com/jkroepke/helm-secrets --version v4.5.1"
                         //sh "helm plugin install https://github.com/aslafy-z/helm-git --version 0.15.1"
+                        writeFile file: "kubeconfig", text: "${kubeconfig}"
                         sh "helm plugin list"
-                    withEnv(["KUBECONFIG=${kubeconfig}"]){
                         sh "printenv"
-                        sh "cat ${kubeconfig}"
                         sh "helmfile sync"
-                    }
                     }
                 }
             }
