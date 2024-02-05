4bea70905abad1a04200d1465063ac6169670218
artem <artem@MacBook-Air-artem.local>
2024-02-05 20:31:39 +0300

    dash

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -3,28 +3,22 @@
 pipeline {
     agent any
 
-    parameters { 
-        choice(name: 'TERRAFORM_OPTS', choices: ['plan', 'apply', 'destroy'], description: '')
-    }
     options {
         ansiColor('xterm')
         timeout(time: 1, unit: 'HOURS') 
     }
     environment {
-        AWS_ACCESS_KEY_ID     = credentials('s3-terraform-secret-key-id')
-        AWS_SECRET_ACCESS_KEY = credentials('s3-terraform-secret-access-key')
-        TF_CLI_CONFIG_FILE = "terraform.tfrc"
-        TF_IN_AUTOMATION      = '1'
+        BOOKINFO_HUB = "cr.yandex/crpmugbv6k1atn1lldsi"
+        BOOKINFO_TAG = "v1"
     }    
     stages {
         stage("build") {
             steps {
-               dir('terraform') {
-                    withCredentials([file(credentialsId: 'terraform_key_sa', variable: 'terraform_key_sa')]) {
-                        writeFile(file: "terraform_key_sa.json", text: "${terraform_key_sa}")
-                        sh "terraform init -upgrade -input=false"
-                        sh "terraform ${TERRAFORM_OPTS} -auto-approve"
-                    }
+               dir('bookinfo_src') {
+                    withCredentials([file(credentialsId: 'docker_secret', variable: 'docker_token')]) {
+                        sh "cat ${docker_token} | docker login --username json_key --password-stdin cr.yandex"
+                        sh "./build-services.sh --push"
+                    }                    
                }
             }
         }
