07cfc6c505f4a09976c9d2ad7acbcee309bc3541
artem <artem@MacBook-Air-artem.local>
2024-02-09 00:16:51 +0300

    pv 5gb

--- jenkins/helm_app.groovy
+++ jenkins/helm_app.groovy
@@ -17,6 +17,7 @@ pipeline {
                         sh "gpg --batch --allow-secret-key-import --import ${helm_gpg_private}"
                         writeFile file: "kubeconfig", text: "${kubeconfig}"
                         sh "helm plugin list"
+                        sh "helm repo updatega"
                         sh "helm secrets upgrade --wait  --install bookinfo . -f ./secret.yaml --namespace bookinfo --create-namespace"
                     }
                 }
