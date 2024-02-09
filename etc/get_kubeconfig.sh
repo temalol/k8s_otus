9f46ba81b373203f2f10c8ad4c918bbd0c485019
artem <artem@MacBook-Air-artem.local>
2024-02-09 23:36:52 +0300

    pv 5gb

--- etc/get_kubeconfig.sh
+++ etc/get_kubeconfig.sh
@@ -52,6 +52,19 @@ mkdir s3_csi_cred
 
 yc iam access-key create  --service-account-name bucket > s3_csi_cred/csi_s3_secret.yaml
 
-###################################
+key_id=$(yq .access_key.key_id < s3_csi_cred/csi_s3_secret.yaml)
+secret=$(yq .secret < s3_csi_cred/csi_s3_secret.yaml)
+
+yq  -n '(.secret.accessKey=strenv(key_id), .secret.secretKey=strenv(secret))' > s3_csi_cred/file.yaml
+sops -p E48E7EBD5FDA8F68CE04FBEB8D3A327416AA7D97 -e s3_csi_cred/file.yaml > s3_csi_cred/prometheus_secret.yaml
+
+##########################################
+################# docker credentials
+mkdir docker_cred
+
+yc iam key create --service-account-name docker-account -o docker_cred/docker_key.json
+
+#####################
+
 
 
